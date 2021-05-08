2c9ba9c3b0b657947ab8aacf9ea1b1de8f29afae
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 15:11:35 +0300

    Define getVenueInfo function

--- functions/getVenueInfo.js
+++ functions/getVenueInfo.js
@@ -2,6 +2,16 @@ require("dotenv").config({ path: `${__dirname}/.env` });
 
 const fetch = require("node-fetch");
 
+const getVenueInfo = async (id) => {
+  const res = await fetch(`https://api.yelp.com/v3/businesses/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.YELP_API}`,
+    },
+  });
+  return res.json();
+};
+
 exports.handler = async ({ queryStringParameters }) => {
   const { id } = queryStringParameters;
 
