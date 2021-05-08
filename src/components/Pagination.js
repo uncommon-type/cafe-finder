da63d50e7ce7a6ddfb573ff137e1c2dae2ef13d9
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 18:56:27 +0300

    Hook up 'onClick' events to 'Next Page' and 'Previous Page' buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const Pagination = ({ total, limit, offset }) => (
+const Pagination = ({ total, limit, offset, setOffset }) => (
   <div className="card-group__pagination">
     {(offset + 1) * limit <= total ? (
-      <button className="btn-next">Next page</button>
+      <button onClick={() => setOffset(offset + limit)} className="btn-next">
+        Next page
+      </button>
     ) : null}
-
-    {offset > 0 ? <button className="btn-prev">Previous page</button> : null}
+    {offset > 0 ? (
+      <button onClick={() => setOffset(offset - limit)} className="btn-prev">
+        Previous page
+      </button>
+    ) : null}
   </div>
 );
 
