0553a132605041d9818247b617fd035a89a56952
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 20:20:29 +0300

    Import clsx and Context

--- src/components/ShortlistButton.js
+++ src/components/ShortlistButton.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 
+import clsx from "clsx";
+import Context from "../Context";
 import HeartIcon from "./HeartIcon";
 
 const ShortlistButton = ({ onClick, id }) => {
