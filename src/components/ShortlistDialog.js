2df3a261f7ebe3c9fbcff0b8533755701cf5da17
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 20:37:51 +0300

    Update jsx

--- src/components/ShortlistDialog.js
+++ src/components/ShortlistDialog.js
@@ -6,15 +6,26 @@ import Context from "../Context";
 
 const ShortlistDialog = ({ closeShortlist }) => {
   const { shortlist } = useContext(Context);
+
   return (
     <div className="shortlist-dialog">
       <div className="shortlist-dialog__inner">
-        <button className="shortlist-dialog__button">
+        <button onClick={closeShortlist} className="shortlist-dialog__button">
           <CloseIcon />
         </button>
-        <ul className="shortlist-dialog__list">
-          <Card />;
-        </ul>
+
+        {shortlist.length === 0 ? (
+          <p className="shortlist-dialog__inner__msg">
+            Your shortlist is empty{" "}
+          </p>
+        ) : (
+          <ul className="shortlist-dialog__list">
+            {shortlist.map((item) => {
+              const feature = { properties: item };
+              return <Card key={item.id} feature={feature} />;
+            })}
+          </ul>
+        )}
       </div>
     </div>
   );
