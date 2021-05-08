0136d4267c0fb43c05957e69a9ec704fe10237ca
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 14:58:19 +0300

    Set up a handler

--- functions/search.js
+++ functions/search.js
@@ -1,3 +1,10 @@
 require("dotenv").config({ path: `${__dirname}/.env` });
 
 const fetch = require("node-fetch");
+
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: "",
+  };
+};
