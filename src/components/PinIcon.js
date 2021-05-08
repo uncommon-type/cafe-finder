cb8f3e42f510e56b1c2a27718f0d23a6f7990844
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 20:01:57 +0300

    Give style prop to PlaceTwoToneIcon

--- src/components/PinIcon.js
+++ src/components/PinIcon.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PlaceTwoToneIcon from "@material-ui/icons/PlaceTwoTone";
 
-const PinIcon = ({ size, color }) => <PlaceTwoToneIcon />;
+const PinIcon = ({ size, color }) => (
+  <PlaceTwoToneIcon style={{ fontSize: size, color }} />
+);
 
 export default PinIcon;
