d4833fb477e1535543ead4a4b35704e961dc7acf
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 14:48:11 +0300

    Instantiate SearchBar and ViewSearchResults components

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
+import SearchBar from "./containers/SearchBar";
+import ViewSearchResults from "./containers/ViewSearchResults";
+
 const Root = () => {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <SearchBar initialSearch="london" />
+      <main>
+        <article className="app">
+          <ViewSearchResults initialSearch="london" />
+        </article>
+      </main>
+    </Router>
+  );
 };
 
 export default Root;
