6b45a23c2b05979eae0064f2de69ac201aff2f3c
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 18:36:20 +0300

    Ifoffset is greater than zero’, then we display ‘previous page’ button,  otherwise,  show null

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -3,7 +3,8 @@ import React from "react";
 const Pagination = ({ total, limit, offset }) => (
   <div className="card-group__pagination">
     <button className="btn-next">Next page</button>
-    <button className="btn-prev">Previous page</button>
+
+    {offset > 0 ? <button className="btn-prev">Previous page</button> : null}
   </div>
 );
 
