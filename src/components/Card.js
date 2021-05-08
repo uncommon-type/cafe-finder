3c1daff44649abd68c96a918c0efc5a0e728e31f
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 19:36:47 +0300

    Update jsx

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,16 +5,25 @@ import ShortlistButton from "./ShortlistButton";
 const Card = ({ feature, handleHover }) => {
   const { id, name, rating, location, url } = feature.properties;
   return (
-    <li className="card">
+    <li
+      onMouseEnter={handleHover ? () => handleHover(id) : null}
+      onMouseLeave={handleHover ? () => handleHover(null) : null}
+      className="card"
+    >
       <div className="card__body">
         <h2>
-          <a href="">Monmouth Coffee</a>
+          <a href={feature.properties.url}>{name}</a>
         </h2>
-        <p>London WC2H 9EU</p>
+        <p>
+          {location.city} {""}
+          {location.zip_code}
+        </p>
         <div className="card__footer">
-          <small>Rating 4.5</small>
+          <small>Rating {rating}</small>
           <div className="card__footer__heart-btn">
-            <ShortlistButton />
+            <button className="heart__button">
+              <HeartIcon />
+            </button>
           </div>
         </div>
       </div>
