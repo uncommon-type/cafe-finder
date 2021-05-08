a07cac3a60ea633054f5520706d5aa89c055d044
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 18:32:22 +0300

    Capture total, limit, offset props

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Pagination = () => (
+const Pagination = ({ total, limit, offset }) => (
   <div className="card-group__pagination">
     <button className="btn-next">Next page</button>
     <button className="btn-prev">Previous page</button>
