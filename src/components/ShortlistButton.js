91399f22672e1280eb1a8668d948b9197a9b06f0
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 20:21:05 +0300

    Get shortlist out of useContext hook

--- src/components/ShortlistButton.js
+++ src/components/ShortlistButton.js
@@ -5,6 +5,7 @@ import Context from "../Context";
 import HeartIcon from "./HeartIcon";
 
 const ShortlistButton = ({ onClick, id }) => {
+  const { shortlist } = useContext(Context);
   return (
     <div className="heart">
       <button className="heart__button">
