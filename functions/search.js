8f66d5fd3e56a061861e2ba5acb418b58bf95d48
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-05-08 15:01:56 +0300

    Define fetchFromYelp function

--- functions/search.js
+++ functions/search.js
@@ -2,7 +2,22 @@ require("dotenv").config({ path: `${__dirname}/.env` });
 
 const fetch = require("node-fetch");
 
-exports.handler = async () => {
+const fetchFromYelp = async (location) => {
+  const res = await fetch(
+    `https://api.yelp.com/v3/businesses/search?term=coffee&location=${location}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.YELP_API}`,
+      },
+    }
+  );
+  return res.json();
+};
+
+exports.handler = async ({ queryStringParameters }) => {
+  const { location } = queryStringParameters;
+
   return {
     statusCode: 200,
     body: "",
